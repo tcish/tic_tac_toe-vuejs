1c4a00ef7fcf87ac136820d61ae1ec175e750525
tcish <khaladkhan250@gmail.com>
2023-06-29 09:31:56 +0600

    added full

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,13 +1,90 @@
 <template>
-  <h1>Hello, World!</h1>
+  <main class="text-center dark:bg-gray-800 dark:text-white">
+    <h1 class="mb-8 text-3xl font-bold uppercase">TIC TAC TOE</h1>
+
+    <h3 class="text-xl mb-4">Player {{ player }}'s turn</h3>
+
+    <div class="flex flex-col items-center mb-8">
+      <div v-for="(row, x) in board" :key="x" class="flex">
+        <div
+          v-for="(cell, y) in row"
+          :key="y"
+          @click="makeMove(x, y)"
+          :class="`border border-white w-20 h-20 hover:bg-gray-700 flex justify-center items-center material-icons-outlined text-4xl cursor-pointer ${
+            cell === 'X' ? 'text-pink-500' : 'text-blue-400'
+          }`"
+        >
+          {{ cell === "X" ? "close" : cell === "O" ? "circle" : "" }}
+        </div>
+      </div>
+    </div>
+
+    <h2 v-if="winner" class="text-6xl font-bold mb-8">
+      Player '{{ winner }}' wins!
+    </h2>
+
+    <button
+      @click="reset"
+      class="px-4 py-2 bg-pink-500 rounded uppercase font-bold hover:bg-pink-600 duration-300"
+    >
+      Reset Game
+    </button>
+  </main>
 </template>
 
-<script>
-export default {
+<script setup>
+import { ref, computed } from "vue";
 
-}
+const player = ref("X");
+const board = ref([
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""],
+]);
+let winner = ref("");
+
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+};
+
+winner = computed(() => calculateWinner(board.value.flat()));
+
+const makeMove = (x, y) => {
+  if (winner.value) return;
+
+  if (board.value[x][y] !== "") return;
+
+  board.value[x][y] = player.value;
+
+  player.value = player.value === "X" ? "O" : "X";
+};
+
+const reset = () => {
+  board.value = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+  player.value = "X";
+};
 </script>
 
 <style>
-
 </style>
